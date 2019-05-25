6989966604b85b541121509bbf1c8f145d60d132
Dylan <dylan.castro@utec.edu.pe>
2019-05-25 17:24:54 -0500

    RF terminado

--- RandomFile.cpp
+++ RandomFile.cpp
@@ -27,54 +27,53 @@ class RandomIndex
 {
 public:
     map<string, int> indice;
+    map<string, int> temp_indice;
 };
 
 class RandomFile
 {
 private:
     string filename;
-    char indexname[50];
 
 public:
     RandomIndex index;
     RandomFile(string filename)
     {
         this->filename = filename;
-        leerIndice();
+        //leerIndice();
     }
-    int busqueda(char indexname[50])
+    void busqueda(char buscar[50])
     {
         Registros *temp = new Registros;
         //this->indexname = indexname;
         ifstream file;
-        file.open(filename, ios::in | ios::binary);
-        char indice;
-        int direccion = 0;
+        file.open("Database/BD2.dat", ios::in | ios::binary);
+        //char indice;
+        //int direccion = 0;
         if (file.is_open())
         {
-            file.seekg(index.indice[indexname], ios::beg);
+            file.seekg(index.indice[buscar], ios::beg);
+            //cout << index.indice[buscar] << endl;
             /*int tam = 0;
             file.read((char *)&tam, sizeof(int));
             file.read((char *)temp, tam);*/
-            file.read((char *)&indice, 50);
-            file.read((char *)&direccion, sizeof(int));
+            //file.read((char *)&indice, 50);
+            file.read((char *)temp, 226);
         }
-        /*cout << temp->id << " ";
-        cout << temp->App << " ";
-        cout << temp->Category << " ";
-        cout << temp->Rating << " ";
-        cout << temp->Review << " ";
-        cout << temp->Size << " ";
-        cout << temp->Installs << " ";
-        cout << temp->Type << " ";
-        cout << temp->Price << " ";
-        cout << temp->Content_Rating << " ";
-        cout << temp->Genres << " ";
-        cout << temp->Last_Updates << " ";
-        cout << temp->Current_Ver << " ";
-        cout << temp->Android_Ver << endl;*/
+        cout << temp->App << " || ";
+        cout << temp->Category << " || ";
+        cout << temp->Rating << " || ";
+        cout << temp->Review << " || ";
+        cout << temp->Size << " || ";
+        cout << temp->Installs << " || ";
+        cout << temp->Type << " || ";
+        cout << temp->Price << " || ";
+        cout << temp->Content_Rating << " || ";
+        cout << temp->Genres << " || ";
+        cout << temp->Last_Updates << " || ";
+        cout << temp->Current_Ver << " || ";
+        cout << temp->Android_Ver << endl;
         delete temp;
-        return direccion;
     };
     void leerIndice()
     {
@@ -84,7 +83,7 @@ public:
         int tamanio_fichero = file.tellg();
         file.seekg(0, ios::beg);
         int tamanio_actual = 0;
-        cout << tamanio_fichero << endl;
+        //cout << tamanio_fichero << endl;
         if (file.is_open())
         {
             while (tamanio_actual != tamanio_fichero)
@@ -94,8 +93,7 @@ public:
                 file.read((char *)&indice, 50);
                 //strcpy(indice, &temp_indice[0]);
                 //string temp_indice(indice);
-                string temp_indice = indice;
-                cout << temp_indice << endl;
+                //string temp_indice = indice;
                 file.read((char *)&direccion, sizeof(int));
                 index.indice[indice] = direccion;
                 tamanio_actual += 54;
@@ -109,12 +107,16 @@ public:
         file.open("Indice/index.dat", ios::out | ios::app | ios::binary);
         if (file.is_open())
         {
-            for (std::map<string, int>::iterator it = index.indice.begin(); it != index.indice.end(); ++it)
+            for (std::map<string, int>::iterator it = index.temp_indice.begin(); it != index.temp_indice.end(); ++it)
             {
-                file.write((char *)&(it->first), 50);
+                char App[50];
+                strcpy(App, (it->first.substr(0, 49)).c_str());
+                file.write((char *)&(App), 50);
                 file.write((char *)&(it->second), sizeof(int));
+                index.indice[App] = it->second;
             }
             file.close();
+            index.temp_indice.clear();
         }
     };
     void imprimirIndices()
@@ -129,7 +131,7 @@ class Database
 {
 private:
     char filename[100];
-    vector<Registros *> registros;
+    vector<Registros> registros;
 
 public:
     void load(RandomFile &randomfile)
@@ -149,20 +151,21 @@ public:
                 int tam = 226;
                 //file.read((char *)&tam, sizeof(int));
                 file.read((char *)registro, tam);
-                registros.push_back(registro);
-                randomfile.index.indice[registro->App] = tamanio_actual;
+                registros.push_back(*registro);
+                //string gg(registro->App);
+                //randomfile.index.temp_indice[registro->App] = tamanio_actual;
                 tamanio_actual += 226;
             }
             file.close();
         }
-        imprimir_alumnos();
+        //imprimir_alumnos();
     };
     void add(RandomFile &randomfile)
     {
         Registros *registro = new Registros;
         cout << "Ingrese el App[50], Category[19], Rating,(float) Review(int), Size[18], Installs[12], Type[4], Price[7], Content_Rating[10], Genres[40], Last_Update[18], Current_Ver[20], Android_Ver[20]" << endl;
         cin >> registro->App >> registro->Category >> registro->Rating >> registro->Review >> registro->Size >> registro->Installs >> registro->Type >> registro->Price >> registro->Content_Rating >> registro->Genres >> registro->Last_Updates >> registro->Current_Ver >> registro->Android_Ver;
-        registros.push_back(registro);
+        registros.push_back(*registro);
         fstream file;
         file.open("Database/BD2.dat", ios::out | ios::app | ios::binary);
         if (file.is_open())
@@ -171,26 +174,26 @@ public:
             //file.write((char *)&tam, sizeof(int));
             file.write((char *)registro, tam);
             file.close();
-            randomfile.index.indice[registro->App] = (registros.size() - 1) * tam;
+            randomfile.index.temp_indice[registro->App] = (registros.size() - 1) * tam;
         }
     }
     void imprimir_alumnos()
     {
         for (int x = 0; x < registros.size(); x++)
         {
-            cout << registros[x]->App << " ";
-            cout << registros[x]->Category << " ";
-            cout << registros[x]->Rating << " ";
-            cout << registros[x]->Review << " ";
-            cout << registros[x]->Size << " ";
-            cout << registros[x]->Installs << " ";
-            cout << registros[x]->Type << " ";
-            cout << registros[x]->Price << " ";
-            cout << registros[x]->Content_Rating << " ";
-            cout << registros[x]->Genres << " ";
-            cout << registros[x]->Last_Updates << " ";
-            cout << registros[x]->Current_Ver << " ";
-            cout << registros[x]->Android_Ver << endl;
+            cout << registros[x].App << " || ";
+            cout << registros[x].Category << " || ";
+            cout << registros[x].Review << " || ";
+            cout << registros[x].Rating << " || ";
+            cout << registros[x].Size << " || ";
+            cout << registros[x].Installs << " || ";
+            cout << registros[x].Type << " || ";
+            cout << registros[x].Price << " || ";
+            cout << registros[x].Content_Rating << " || ";
+            cout << registros[x].Genres << " || ";
+            cout << registros[x].Last_Updates << " || ";
+            cout << registros[x].Current_Ver << " || ";
+            cout << registros[x].Android_Ver << endl;
         }
     };
 };
@@ -198,11 +201,15 @@ int main()
 {
     Database db;
     RandomFile randomfile("Indice/index.dat");
-    //db.load(randomfile);
+    randomfile.leerIndice();
+    db.load(randomfile);
+    db.imprimir_alumnos();
+    //randomfile.busqueda("Coloring book moana");
+    //randomfile.guardarIndice();
+    //randomfile.imprimirIndices();
+    //db.add(randomfile);
     //db.imprimir_alumnos();
     //randomfile.imprimirIndices();
-    //randomfile.guardarIndice();
-    //db.add(randomfile);
     //randomfile.guardarIndice();
     /*int add_alumnos;
     cin >> add_alumnos;
