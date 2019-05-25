6b4433afad5046568163479dceba7f0b875cfd79
Nicolás Chipana <34188439+NChipanaUTEC@users.noreply.github.com>
2019-05-25 17:18:16 -0500

    Dynamic Hashing implementado

---
+++ DB/Block.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <fstream>
+#include <cmath>
+#define WIDTH 2
+using namespace std;
+
+
+class Block{
+private:
+  string file;
+  vector<long> values;
+public:
+  Block(string name):name(name){}
+
+  string getName(){return name;}
+
+  int getSize(){return values.size();}
+
+  void print(){
+    for(auto item:values){cout <<item<<" ";}
+    cout <<"\n";
+  }
+
+  vector<long> getValues(){return values;}
+
+  bool insert(long registerNumber){
+    values.push_back(registerNumber);
+    return true;
+  }
+
+};
