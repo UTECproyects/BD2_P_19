1f2638186eb9e6caa59f6d67e64a1ed371d4ea1f
Nicolás <nicolas.chipana@utec.edu.pe>
2019-05-25 19:53:33 -0500

    Dynamic Hashing funcionando en main.cpp

--- DB/dynamicHashing.h
+++ DB/dynamicHashing.h
@@ -3,6 +3,18 @@ using namespace std;
 
 map<string,Block*> indexTable;
 
+string hashString(string name){
+  long hash = 0;
+  for(int i = 0; i < name.size(); i++){
+    hash += (int)name[i]*(int)name[i];
+  }
+  hash = hash%1024;
+  string result = bitset< 10 >(hash).to_string();
+  return result;
+}
+
+void divideBlock(Block* oldblock);
+
 void initializeTable(){
   Block* cero = new Block("0");
   Block* uno = new Block("1");
@@ -14,6 +26,32 @@ void initializeTable(){
     if(last == "0"){indexTable.insert(pair<string,Block*>(hash,cero));}
     else{indexTable.insert(pair<string,Block*>(hash,uno));}
   }
+}
+
+void insertToTable(string name, long registerNumber){
+  string binaryHash = hashString(name);
+  if(indexTable.size()==0){initializeTable();}
+  if(indexTable[binaryHash]-> getSize()<WIDTH){
+    indexTable[binaryHash]-> insert(registerNumber);
+  }
+  else{
+    divideBlock(indexTable[binaryHash]);
+    insertToTable(binaryHash,registerNumber);
+  }
+}
+
+Registros searchInTable(string name){
+  string binaryHash = hashString(name);
+  Registros reg = indexTable[binaryHash]->searchForValue(name);
+  return reg;
+}
+
+void printTable(){
+    for(auto item: indexTable){
+      if(item.second)
+        cout<< item.first<<" "<<item.second->getName()<<" ";
+        item.second->print();
+    }
 }
 
 void divideBlock(Block* oldblock){
@@ -30,35 +68,11 @@ void divideBlock(Block* oldblock){
     else if(hash.substr(10-(newname0.size()),newname0.size())==newname1)
       indexTable[hash] = block1;
   }
+  vector<long> values = oldblock->getValues();
+  for(int i = 0;i < values.size();i++){
+    Registros reg;
+    reg.get_registro(values[i]);
+    insertToTable(reg.App,values[i]);
+  }
   delete oldblock;
-}
-
-string hashString(string name){
-  long hash = 0;
-  for(int i = 0; i < name.size(); i++){
-    hash += (int)name[i]*(int)name[i];
-  }
-  hash = hash%1024;
-  string result = bitset< 10 >(hash).to_string();
-  return result;
-}
-
-void insertToTable(string name, long registerNumber){
-  string binaryHash = hashString(name);
-  if(indexTable.size()==0){initializeTable();}
-  if(indexTable[binaryHash]-> getSize()<WIDTH){
-    indexTable[binaryHash]-> insert(registerNumber);
-    cout << "Inserted "<<registerNumber<<" in Block no."<<indexTable[binaryHash]->getName()<<"\n";
-  }
-  else{
-    divideBlock(indexTable[binaryHash]);
-    insertToTable(binaryHash,registerNumber);
-  }
-}
-
-void printTable(){
-    for(auto item: indexTable){
-      if(item.second)
-        cout<< item.first<<" "<<item.second->getName()<<"\n";
-    }
 }
--- DB/registros.h
+++ DB/registros.h
@@ -6,7 +6,7 @@
 using namespace std;
 class Registros
 {
-public:    
+public:
     char App[50];
     char Category[19];
     float Rating;
@@ -21,12 +21,26 @@ public:
     char Current_Ver[20];
     char Android_Ver[20];
 
-    void get_registro(int pos,Registros *regis){
-        fstream files;        
-        files.open("BD2.dat",ios::in | ios::binary);
+    void get_registro(int pos){
+        fstream files;
+        files.open("DB/BD2.dat",ios::in | ios::binary);
         files.seekg(226*pos,ios::beg);
-        files.read((char *)regis, 226);
+        files.read((char *)this, 226);
         files.close();
+    }
+
+    int getDocumentSize(){
+      fstream file;
+      file.open("DB/BD2.dat",ios::in | ios::binary);
+      file.seekg(0, ios::end);
+      int sizeDocument = file.tellg();
+      return sizeDocument/226;
+    }
+
+    void print(){
+      cout<<this->App<<"|"<<this->Category<<"|"<<this->Rating<<"|"<<this->Review<<"|"<<this->Size<<"|"<<this->Installs<<"|";
+      cout<<this->Type<<"|"<<this->Price<<"|"<<this->Content_Rating<<"|"<<this->Genres<<"|"<<this->Last_Updates<<"|";
+      cout<<this->Current_Ver<<"|"<<this->Android_Ver<<"\n";
     }
 };
 
