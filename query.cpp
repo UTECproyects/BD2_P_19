2777358880aaf56b1dab8b9b0cc65538f386424f
Dylan <dylan.castro@utec.edu.pe>
2019-05-25 20:02:18 -0500

    as

--- query.cpp
+++ query.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include "RandomFile.h"
 using namespace std;
 class query
 {
@@ -14,13 +15,23 @@ public:
     query()
     {
         consultas_aceptadas.push_back("select");
-        columnas.push_back("*");
-        columnas.push_back("varian_apesta");
         consultas_aceptadas.push_back("from");
         consultas_aceptadas.push_back("DB");
         consultas_aceptadas.push_back("where");
-        columnas.push_back("a");
-        columnas.push_back("b");
+        columnas.push_back("*");
+        columnas.push_back("App");
+        columnas.push_back("Category");
+        columnas.push_back("Rating");
+        columnas.push_back("Review");
+        columnas.push_back("Size");
+        columnas.push_back("Installs");
+        columnas.push_back("Type");
+        columnas.push_back("Price");
+        columnas.push_back("Content_Rating");
+        columnas.push_back("Genres");
+        columnas.push_back("Last_Updates");
+        columnas.push_back("Current_Ver");
+        columnas.push_back("Android_Ver");
     }
     ~query() {}
     bool verificar_consulta_recibida(vector<string> query)
@@ -29,19 +40,23 @@ public:
 
         for (size_t i = 0; i < query.size(); i++)
         {
-            if (i == 1)
+            if (i == 1 || i == 5)
             {
                 if (find(columnas.begin(), columnas.end(), query[i]) != columnas.end())
                 {
                     contador_de_validacion++;
                 }
             }
-            else if (i == 5)
+            else if (i == 6)
             {
-                if (analizar_sexto_parametro(query[i]))
+                if (analizar_operador_logico(query[i]))
                 {
                     contador_de_validacion++;
                 }
+            }
+            else if (i == 7)
+            {
+                contador_de_validacion++;
             }
             else if (find(consultas_aceptadas.begin(), consultas_aceptadas.end(), query[i]) != consultas_aceptadas.end())
             {
@@ -63,42 +78,42 @@ public:
         }
         return false;
     }
-    string analizar_operador_logico(string param)
+    bool analizar_operador_logico(string param)
     {
         for (size_t i = 0; i < param.size(); i++)
         {
             if (param[i] == '=')
             {
-                return "=";
+                return true;
             }
             else if (param[i] == '<')
             {
-                return "<";
+                return true;
             }
             else if (param[i] == '>')
             {
-                return ">";
+                return true;
             }
             else if (param[i] == '<' && param[i + 1] == '=')
             {
-                return "<=";
+                return true;
             }
             else if (param[i] == '>' && param[i + 1] == '=')
             {
-                return ">=";
+                return true;
             }
             else if (param[i] == '!' && param[i + 1] == '=')
             {
-                return "!=";
+                return true;
             }
         }
     }
     bool analizar_sexto_parametro(string param)
     {
-        string operador_logico = analizar_operador_logico(param);
+        bool operador_logico = analizar_operador_logico(param);
         string a = param.substr(0, param.find(operador_logico));
-        string b = param.substr(param.find(operador_logico) + operador_logico.size());
-        if (revisar_si_columna_existe_en_BD(a) && revisar_si_columna_existe_en_BD(b))
+        //string b = param.substr(param.find(operador_logico) + operador_logico.size());
+        if (revisar_si_columna_existe_en_BD(a))
         {
             return true;
         }
@@ -111,20 +126,25 @@ public:
             cout << "Error en la consulta, consulta erronea" << endl;
             return false;
         }
+        RandomFile randomfile("Indice/index.dat");
+        randomfile.leerIndice();
+        //randomfile.imprimirIndices();
         if (query.size() == 4)
         {
-            cout << "Mostrar " + query[1] + " de la base de datos " + query[3] << endl;
+            randomfile.busqueda("0", query[1]);
         }
-        else if (query.size() == 6)
+        else if (query.size() == 8)
         {
-            cout << "Mostrar " + query[1] + " de la base de datos, donde " + query[5] << endl;
+            char App[50];
+            strcpy(App, (query[7].substr(0, 49)).c_str());
+            randomfile.busqueda(App, query[1]);
         }
         return true;
     }
 };
 int main()
 {
-    query obj;
+    /*query obj;
     vector<string> query;
     string word;
     while (true)
@@ -132,9 +152,43 @@ int main()
         cin >> word;
         if (word == ";")
             break;
-
+        if (word[0] == (char)34)
+        {
+            bool cadena_analizada = false;
+            word = word.substr(1);
+            string sub_cadena;
+            if (word[word.length() - 1] == (char)34)
+            {
+                word = word.substr(0, word.length() - 1);
+                cadena_analizada = true;
+            }
+            while (!cadena_analizada)
+            {
+                cin >> sub_cadena;
+                if (sub_cadena[sub_cadena.length() - 1] == (char)34)
+                {
+                    sub_cadena = sub_cadena.substr(0, sub_cadena.length() - 1);
+                    cadena_analizada = true;
+                }
+                word += " " + sub_cadena;
+            }
+        }
+        //cout << word << endl;
         query.push_back(word);
     }
     obj.ejecutar_consulta_recibida(query);
-    query.clear();
+    query.clear();*/
+    Database db;
+    RandomFile randomfile("Indice/index.dat");
+    db.load(randomfile);
+    //randomfile.leerIndice();
+    db.imprimir_DB();
+    //randomfile.busqueda("Coloring book moana");
+    randomfile.guardarIndice();
+    //randomfile.imprimirIndices();
+    //db.add(randomfile);
+    //db.imprimir_alumnos();
+    //randomfile.imprimirIndices();
+    //randomfile.guardarIndice();
+    return 0;
 }
